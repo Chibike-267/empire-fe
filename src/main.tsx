6053dc9fe5cb08e501012f788834ec3d76ed8655
Chibike-267 <paulinuschukwubuikem98@gmail.com>
2024-08-02 12:45:21 +0100

    job

---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+//import ReactDOM from 'react-dom/client';
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import App from "./App.tsx";
+import "./index.css";
+
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <Router>
+          <App />
+        </Router>
+      </Provider>
+    </React.StrictMode>
+  );
+}
